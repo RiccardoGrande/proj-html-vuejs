e39592054b1d3cd5daa9915d4b299ec77a30ee94
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-26 19:18:08 +0200

    add recruit

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,19 @@ export default {
 
 <template>
   <AppHeader></AppHeader>
+  <section class="reclutement">
+    <div class="container">
+      <div class="row">
+        <div class="col text-end">
+          <h4>How to Enroll Your Child to a Class?</h4>
+        </div>
+        <div class="col d-flex align-items-center justify-content-center">
+          <a href="h"></a>
+          <img src="fable/hover-button2.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped>
@@ -21,5 +34,18 @@ export default {
   margin: 0;
   box-sizing: border-box;
 
+}
+
+.reclutement {
+  background-image: url('fable/images/pattern.png');
+}
+
+h4 {
+  padding: 1rem;
+  color: white;
+}
+
+img {
+  height: 60px;
 }
 </style>
