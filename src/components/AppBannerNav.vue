6492e252b2f0e465b8cf96cccc34f95550725903
RiccardoGrande <grandericcardo1997@gmail.com>
2023-04-26 13:00:20 +0200

    add components nav banner

---
+++ src/components/AppBannerNav.vue
@@ -0,0 +1,97 @@
+<script>
+export default {
+    name: 'AppBannerNav',
+    data() {
+
+        return {
+
+            banner: [
+                {
+                    image: 'fable/home.png',
+                    text: 'Home',
+
+
+                },
+                {
+                    image: 'fable/document.png',
+                    text: 'Pages',
+
+
+                },
+                {
+                    image: 'fable/printer.png',
+                    text: 'Blog',
+
+
+                },
+                {
+                    image: 'fable/cart.png',
+                    text: 'Shop',
+
+
+                },
+                {
+                    image: 'fable/lab.png',
+                    text: 'Shortcodes',
+
+
+                },
+                {
+                    image: 'fable/chat.png',
+                    text: 'Support',
+
+
+                },
+                {
+                    image: 'fable/envelope.png',
+                    text: 'Contact',
+
+
+                }
+            ]
+
+        }
+
+    }
+
+}
+</script>
+
+
+<template>
+    <section>
+        <div class="container">
+            <div class="row p-1">
+                <div class="col-4 d-flex align-items-center justify-content-center">
+                    <img class="logo" src="/fable/images/logo.png" alt="fable_logo">
+                </div>
+                <div class="col d-flex flex-column align-items-center justify-content-center" v-for="item in banner">
+                    <img class="icon" :src="item.image">
+                    <span>{{ item.text }}</span>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+</template>
+
+
+
+<style lang="scss" scoped>
+.logo {
+    height: 100px;
+}
+
+.icon {
+    height: 30px;
+    width: 30px;
+}
+
+span {
+    padding-top: 10px;
+    color: #554e9e;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+import AppBannerNav from './AppBannerNav.vue'
+export default {
+    components: {
+        AppBannerNav,
+    }
+
+}
+</script>
+
+
+<template>
+    <AppBannerNav></AppBannerNav>
+</template>
+
+
+
+<style lang="scss" scoped></style>
